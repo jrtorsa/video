d0b0842883361058e5c465fb470d514d41930812
Raul T <jr.torsa@gmail.com>
2020-07-30 14:53:45 -0600

    created search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import SearchBar from "./SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <input />
+        <SearchBar />
       </div>
     );
   }
